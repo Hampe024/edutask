d3659a310a6a83cef994c176150c734cf73ac891
Hampus <hampe024@gmail.com>
2024-04-21 11:52:11 +0100

    unit 1 assignement 2 fix

---
+++ backend/test/unit/test_assignment_2.py
@@ -0,0 +1,82 @@
+from src.controllers.usercontroller import UserController
+import pytest
+import unittest.mock as mock
+from unittest.mock import patch
+from src.util.dao import DAO
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "input_email, DAO_return_value, output_expected",
+    [
+        (
+            "test@email.com",
+            pytest.raises(Exception),
+            Exception
+        ),
+    ]
+)
+def test_get_user_by_email_Exception(input_email, DAO_return_value, output_expected):
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.side_effect = DAO_return_value
+    sut = UserController(dao=mockedDAO)
+
+    with pytest.raises(Exception):
+        usercontrollerresult = sut.get_user_by_email(email=input_email)
+        assert usercontrollerresult == output_expected
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "input_email, DAO_return_value, output_expected",
+    [
+        (
+            "test@email.com",
+            [{"user object"}],
+            {"user object"}
+        ),
+        (
+            "test@email.com",
+            [],
+            None
+        ),
+        (
+            "test@email.com",
+            [{"user object"}, {"other user object"}],
+            {"user object"}
+        ),
+    ]
+)
+def test_get_user_by_email(input_email, DAO_return_value, output_expected):
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = DAO_return_value
+    sut = UserController(dao=mockedDAO)
+
+    usercontrollerresult = sut.get_user_by_email(email=input_email)
+    assert usercontrollerresult == output_expected
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "input_email, DAO_return_value, output_expected",
+    [
+        (
+            "not an email",
+            [],
+            ValueError('Error: invalid email address')
+        ),
+        (
+            "@.com",
+            [],
+            ValueError('Error: invalid email address')
+        ),
+    ]
+)
+def test_get_user_by_email_ValueError(input_email, DAO_return_value, output_expected):
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = DAO_return_value
+    sut = UserController(dao=mockedDAO)
+
+    with pytest.raises(ValueError):
+        usercontrollerresult = sut.get_user_by_email(email=input_email)
+        assert usercontrollerresult == output_expected
+
